4425944f1764124a674f8cffae1ac2a6ac35ceda
liamSy418 <143149425+liamSy418@users.noreply.github.com>
2024-04-09 10:55:27 -0700

    make test paragraph :)

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,11 @@ export default function Home() {
       <div className="rounded-lg bg-blue-500 p-4">
         <h1 className="text-white text-center text-4xl font-black	">Introduction</h1>
       </div>
+      <div className="rounded-lg bg-white p-100">
+        <p className="text-black text-left">
+          This is an example paragraph. Don't forget to google en passant, as it is very unsightreadable for new players.
+        </p>
+      </div>
     </main>
   );
 }
