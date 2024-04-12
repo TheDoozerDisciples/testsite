939a4a5ffbd5fd3a6492cbf7430f06478be381e1
Liam Sy <liamjsy9@gmail.com>
2024-04-11 20:47:23 -0700

    lol

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,8 @@ export default function Home() {
         <h2 className="text-gray-200 text-center text-3xl font-bold">By Cyrus Brown, Peter Du, and Liam Sy</h2>
       </div>
       <div className="flex bg-gradient-to-b from-emerald-300 to-emerald-400 rounded-lg mt-0 w-screen-4 flex-col h-screen mx-4">
-        <p className="text-white text-left text-2xl font-semibold w-7/12">WoW! FIRE IN THE HOLE!!!! GOOGLE EN PASSANT!!! SILKSONG COMES OUT TOMORROW FR!!! BRO SLAY THE SPIRE II IS IN 2025 ISTG 2025 HAS GOT TO BE THE BEST YEAR EVER JUST FOR THAT...</p>
+        <p className="text-white text-left text-2xl font-semibold w-7/12 mx-4 my-4">WoW! FIRE IN THE HOLE!!!! GOOGLE EN PASSANT!!! SILKSONG COMES OUT TOMORROW FR!!! BRO SLAY THE SPIRE II IS IN 2025 ISTG 2025 HAS GOT TO BE THE BEST YEAR EVER JUST FOR THAT...</p>
+        <Image src="/images/dashspider.jpg" alt="idk man this seems too unsightreadable for new players" width={600} height={378}/>
       </div>
     </main>
   );
