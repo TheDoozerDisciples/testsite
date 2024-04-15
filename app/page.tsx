38d523667493ae3bd5b46c7424764bb9d12b5311
Liam Sy <143149425+liamSy418@users.noreply.github.com>
2024-04-15 13:16:41 -0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,15 @@ export default function Home() {
         <h2 className="text-gray-200 text-center text-3xl font-bold">By Cyrus Brown, Peter Du, and Liam Sy</h2>
       </div>
       <div className="flex bg-gradient-to-b from-emerald-300 to-emerald-400 rounded-lg mt-0 w-screen-4 flex-col h-screen mx-4">
-        <p className="text-white text-left text-2xl font-semibold w-7/12 mx-4 my-4 indent-6">As you may know, all life is made up of cells. This project details the inner workings of a plant cell. We interviewed accomplished biologists at UC Davis about the amazing and complex world of plant cell biology.</p>
-        <p className="text-white text-left text-2xl font-semibold w-7/12 mx-4 my-4 indent-6">Every component in the model you see before you is removable. To get started, try placing a part on the [base].</p>
-        <Image src="/images/dashspider.jpg" alt="" className="rounded-lg border-8 border-solid border-green-700 object-right-top mx-4 my-4" width={600} height={378}/>
+        <div>
+          <p className="text-white text-left text-2xl font-semibold w-7/12 mx-4 my-4 indent-6">As you may know, all life is made up of cells. This project details the inner workings of a plant cell. We interviewed accomplished biologists at UC Davis about the amazing and complex world of plant cell biology.</p>
+        </div>
+        <div>
+          <p className="text-white text-left text-2xl font-semibold w-7/12 mx-4 my-4 indent-6">Every component in the model you see before you is removable. To get started, try placing a part on the [base].</p>
+        </div>
+        <div className="object-right-top mx-4 my-4 w-600px height-378px">
+          <Image src="/images/dashspider.jpg" alt="" className="rounded-lg border-8 border-solid border-green-700" width={600} height={378}/>
+        </div>
       </div>
     </main>
   );
