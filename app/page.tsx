98081555915a2635d17dd6565d4c9a827eb680e1
Liam Sy <143149425+liamSy418@users.noreply.github.com>
2024-04-15 13:30:45 -0700

    fix columnt thing probably

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,14 @@ export default function Home() {
         <h2 className="text-gray-200 text-center text-3xl font-bold">By Cyrus Brown, Peter Du, and Liam Sy</h2>
       </div>
       <div className="grid grid-cols-3 grid-rows-2 gap-0 bg-gradient-to-b from-emerald-300 to-emerald-400 rounded-lg mt-0 w-screen-4 h-screen mx-4">
-        <div>
-          <p className="text-white text-left text-2xl font-semibold mx-4 my-4 indent-6 col-span-2">As you may know, all life is made up of cells. This project details the inner workings of a plant cell. We interviewed accomplished biologists at UC Davis about the amazing and complex world of plant cell biology.</p>
+        <div className="col-span-2">
+          <p className="text-white text-left text-2xl font-semibold mx-4 my-4 indent-6">As you may know, all life is made up of cells. This project details the inner workings of a plant cell. We interviewed accomplished biologists at UC Davis about the amazing and complex world of plant cell biology.</p>
         </div>
         <div className="mx-4 my-4">
           <Image src="/images/dashspider.jpg" alt="" className="rounded-lg border-8 border-solid border-green-700" width={600} height={378}/>
         </div>
-        <div>
-          <p className="text-white text-left text-2xl font-semibold mx-4 my-4 indent-6 col-span-2">Every component in the model you see before you is removable. To get started, try placing a part on the [base].</p>
+        <div className="col-span-2">
+          <p className="text-white text-left text-2xl font-semibold mx-4 my-4 indent-6">Every component in the model you see before you is removable. To get started, try placing a part on the [base].</p>
         </div>
       </div>
     </main>
