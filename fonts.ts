e5549e6a814f997e57e18207aed87601d3b0d429
Liam Sy <143149425+liamSy418@users.noreply.github.com>
2024-04-26 10:42:04 -0700

    test futura stuff

---
+++ fonts.ts
@@ -0,0 +1,13 @@
+import localFont from "next/font/local";
+
+// const futura = localFont({
+// 	src: [
+// 		{
+// 			path: "./public/font/futura/FuturaNowHeadline.woff2",
+// 			weight: "400",
+// 			style: "normal"
+// 		}
+// 	]
+// })
+const futura = localFont({src: "./public/font/futura/FuturaNowHeadline.woff2"})
+export { futura }
