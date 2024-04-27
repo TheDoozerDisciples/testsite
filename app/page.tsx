a2a71830daad28f8b4b49a656aa22291d438f397
Liam Sy <liamjsy9@gmail.com>
2024-04-27 15:19:43 -0700

    overhaul aesthetic

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,34 @@ import { futura } from "./fonts.ts"
 // default page, intro
 export default function Home() {
 	return (
-		<main className={futura.className}>
-			<div className="rounded-lg bg-gradient-to-r from-green-600 to-green-400 p-7 mx-4 mb-2 mt-4">
-				<h1 className="text-gray-200 text-center text-6xl font-black">Q4 Bio Project</h1>
-				<h2 className="text-gray-200 text-center text-3xl font-bold">By Cyrus Brown, Peter Du, and Liam Sy</h2>
+		<main className={`${futura.className} flex flex-col h-screen`}>
+			<div className="rounded-lg bg-gradient-to-r from-nice-blue-600 to-nice-blue-500 p-7 mx-4 mb-2 mt-4">
+				<h1 className="text-black text-center text-6xl font-black p-2 underline decoration-3 decoration-nice-purple-700 decoration-double">Q4 Bio Project</h1>
+				<h2 className="text-black text-center text-3xl font-bold mt-2">By Cyrus Brown, Peter Du, and Liam Sy</h2>
 			</div>
-			<div className="grid grid-cols-1 grid-rows-2 gap-0 bg-gradient-to-b from-emerald-300 to-emerald-400 rounded-lg mt-0 w-screen-4 h-screen mx-4">
+			<div className="grid grid-cols-1 grid-rows-2 gap-0 bg-gradient-to-b from-nice-yellow-200 to-nice-yellow-300 rounded-lg mt-0 w-screen-4 flex-grow mx-4">
 				<div className="flex flex-row">
-					<div className="flex-grow flex-auto">
-						<p className="text-white text-left text-2xl font-semibold mx-8 my-8 indent-6">As you may know, all life is made up of cells. This project details the inner workings of a plant cell. We interviewed accomplished biologists at UC Davis about the amazing and complex world of plant cell biology.</p>
+					<div className="flex-grow flex-auto bg-nice-green-500 rounded-lg mx-4 mt-4 mb-2">
+						<p className="text-black text-left text-2xl font-semibold mx-4 my-4 indent-6 leading-loose">
+							As you may know, <a className="font-extrabold text-nice-purple-500">all life is made up of cells</a>. This project details the inner workings of a typical plant cell. 
+							We interviewed accomplished biologists at <a className="font-black text-nice-yellow-500">UC Davis</a> about 
+							the amazing and complex world of <a className=" font-extrabold text-nice-blue-700">plant cell biology</a>.
+							We also created a 3D model of a plant cell to help visualize what we learned.
+						</p>
 					</div>
-					<div className="my-8 mx-8 self-center flex-auto">
-						<Image src="/images/dashspider.jpg" alt="" className="rounded-lg border-8 border-solid border-green-700 object-contain max-h-96 max-w-96" width={500} height={500}/>
+					<div className="mx-4 self-center flex-auto">
+						<Image src="/images/uc_davis_logo.png" alt="" className="rounded-lg border-8 border-solid border-nice-pink-300 object-contain max-h-96 max-w-96" width={400} height={400}/>
 					</div>
 				</div>
 				<div className="flex flex-row">
-					<div className="my-8 mx-8 self-center flex-auto">
-						<Image src="/images/en_passant.png" alt="" className="rounded-lg border-8 border-solid border-green-700 object-contain" width={300} height={300}/>
+					<div className="mx-4 self-center flex-auto">
+						<Image src="/images/plant_cell.jpg" alt="" className="rounded-lg border-8 border-solid border-nice-pink-300 object-contain" width={400} height={400}/>
 					</div>
-					<div className="flex-grow flex-auto">
-						<p className="text-white text-left text-2xl font-semibold mx-4 my-4 indent-6">Every component in the model you see before you is removable. To get started, try placing a part on the [base].</p>
+					<div className="flex-grow flex-auto bg-nice-purple-400 rounded-lg mx-4 mt-2 mb-4">
+						<p className="text-black text-left text-2xl font-semibold mx-4 my-4 indent-6 leading-loose">
+							Every component in the model you see before you is <a className="font-extrabold text-nice-blue-600">removable</a>. 
+							To get started, try placing a part of the cell on the base.
+						</p>
 					</div>
 				</div>
 			</div>
--- app/page_template/page.tsx
+++ app/page_template/page.tsx
@@ -2,28 +2,33 @@ import Image from "next/image";
 import { futura } from "../fonts.ts"
 
 // default page, intro
+// change up the colors every page obviously
 export default function Home() {
 	return (
-		<main className={futura.className}>
-			<div className="rounded-lg bg-gradient-to-r from-green-600 to-green-400 p-7 mx-4 mb-2 mt-4">
-				<h1 className="text-gray-200 text-center text-6xl font-black">TITLE</h1>
-				<h2 className="text-gray-200 text-center text-3xl font-bold">SUBCAPTION</h2>
+		<main className={`${futura.className} flex flex-col h-screen`}>
+			<div className="rounded-lg bg-gradient-to-r from-nice-blue-600 to-nice-blue-500 p-7 mx-4 mb-2 mt-4">
+				<h1 className="text-black text-center text-6xl font-black p-2 underline decoration-3 decoration-nice-purple-600 decoration-double">INSERT TITLE HERE</h1>
+				<h2 className="text-black text-center text-3xl font-bold mt-2">SUBCAPTION</h2>
 			</div>
-			<div className="grid grid-cols-1 grid-rows-2 gap-0 bg-gradient-to-b from-emerald-300 to-emerald-400 rounded-lg mt-0 w-screen-4 h-screen mx-4">
+			<div className="grid grid-cols-1 grid-rows-2 gap-0 bg-gradient-to-b from-nice-blue-200 to-nice-blue-300 rounded-lg mt-0 w-screen-4 flex-grow mx-4">
 				<div className="flex flex-row">
-					<div className="flex-grow flex-auto">
-						<p className="text-white text-left text-2xl font-semibold mx-8 my-8 indent-6">TEXT</p>
+					<div className="flex-grow flex-auto bg-nice-blue-500 rounded-lg mx-4 mt-4 mb-2">
+						<p className="text-black text-left text-2xl font-semibold mx-4 my-4 indent-6 leading-loose">
+							TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT NEED ENOUGH TEXT FOR THIS TO LINE WRAP
+						</p>
 					</div>
-					<div className="my-8 mx-8 self-center flex-auto">
-						<Image src="/images/dashspider.jpg" alt="" className="rounded-lg border-8 border-solid border-green-700 object-contain max-h-96 max-w-96" width={500} height={500}/>
+					<div className=" mx-4 self-center flex-auto">
+						<Image src="/images/dashspider.jpg" alt="" className="rounded-lg border-8 border-solid border-nice-green-600 object-contain max-h-96 max-w-96" width={500} height={500}/>
 					</div>
 				</div>
 				<div className="flex flex-row">
-					<div className="my-8 mx-8 self-center flex-auto">
-						<Image src="/images/en_passant.png" alt="" className="rounded-lg border-8 border-solid border-green-700 object-contain" width={300} height={300}/>
+					<div className=" mx-4 self-center flex-auto">
+						<Image src="/images/en_passant.png" alt="" className="rounded-lg border-8 border-solid border-nice-green-600 object-contain" width={300} height={300}/>
 					</div>
-					<div className="flex-grow flex-auto">
-						<p className="text-white text-left text-2xl font-semibold mx-4 my-4 indent-6">TEXT</p>
+					<div className="flex-grow flex-auto bg-nice-blue-500 rounded-lg mx-4 mt-2 mb-4">
+						<p className="text-black text-left text-2xl font-semibold mx-4 my-4 indent-6 leading-loose">
+							TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT NEED ENOUGH TEXT FOR THIS TO LINE WRAP
+						</p>
 					</div>
 				</div>
 			</div>
