43ae10ce2a6d940074161ce65ff8310049f26661
Liam Sy <liamjsy9@gmail.com>
2024-04-30 18:32:56 -0700

    add navigation buttons to all pages

--- app/page_template/page.tsx
+++ app/page_template/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { futura } from "../fonts.ts"
+import Link from "next/link"
 
 // Page template, used for all pages
 // change up the colors every page obviously
@@ -7,6 +8,7 @@ export default function Home() {
 	return (
 		<main className={`${futura.className} flex flex-col h-screen`}>
 			<div className="rounded-lg bg-gradient-to-r from-nice-blue-600 to-nice-blue-500 p-7 mx-4 mb-2 mt-4">
+			<Link href="/navigation" className="absolute top-2 left-2 rounded-lg bg-black text-nice-purple-600 text-xl mx-4 my-4 object-top-left border-solid border-nice-yellow-300 border-2">Navigation</Link>
 				<h1 className="text-black text-center text-6xl font-black p-2 underline decoration-3 decoration-nice-purple-600 decoration-double">INSERT TITLE HERE</h1>
 				<h2 className="text-black text-center text-3xl font-bold mt-2">SUBCAPTION</h2>
 			</div>
