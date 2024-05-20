f4700c62e2a4515228f4e79f2b46c3bd40f392af
Liam Sy <143149425+liamSy418@users.noreply.github.com>
2024-05-20 12:38:33 -0700

    make the bibliography MORE FLEX (flex is our lord and savior)

--- app/annotated_bibliography/page.tsx
+++ app/annotated_bibliography/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 			<div className="grid grid-cols-1 grid-rows-1 gap-0 bg-gradient-to-b from-nice-blue-200 to-nice-blue-300 rounded-lg mt-0 w-screen-4 flex-grow mx-4">
 				<div className="flex flex-row">
 					<div className="basis-6/12 bg-nice-blue-500 rounded-lg mx-4 mt-4 mb-2"> {/*Sources #1-#10*/}
-						<div className="text-black text-left text-base mx-4 my-2">
+						<div className="text-black text-left text-base mx-4 my-2 flex flex-col">
 							<div className="my-2">{/*Source #1*/}
 								<p>
 								“Chloroplast.” <a className="italic">Funk & Wagnalls New World Encyclopedia</a>, Jan. 2018, p. 1. <a className="italic">EBSCOhost</a>, research.ebsco.com/linkprocessor/plink?id=b7671787-17b8-3725-b420-2d495ab88b0c
@@ -106,7 +106,7 @@ export default function Home() {
 						</div>
 					</div>
 					<div className="basis-6/12 bg-nice-blue-500 rounded-lg mx-4 mt-4 mb-2"> {/*Sources #11-#20*/}
-						<div className="text-black text-left text-base mx-4 my-2">
+						<div className="text-black text-left text-base mx-4 my-2 flex flex-col">
 							<div className="my-2">{/*Source #11*/}
 								<p>
 									The Editors of Encyclopaedia Britannica. "plant cell". <a className="italic">Encyclopedia Britannica</a>, 3 May. 2024, https://www.britannica.com/science/plant-cell. Accessed 30 April 2024.
